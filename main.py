c321e20d4ad266321bdd4fff52d21ded4992e7ea
garret <grumbea@proton.me>
2023-03-26 19:31:23 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,34 @@
+from tkinter import *
+
+
+def calculate():
+    miles = int(user_input.get())
+    km = round(miles * 1.6)
+    calculation.config(text=km)
+
+
+window = Tk()
+window.title("My First GUI Program")
+window.minsize(width=200, height=100)
+window.config(padx=20, pady=20)
+
+user_input = Entry()
+user_input.config(width=10)
+user_input.grid(column=1, row=0)
+
+miles_label = Label(text="Miles")
+miles_label.grid(column=2, row=0)
+
+is_equal_to_label = Label(text="is equal to")
+is_equal_to_label.grid(column=0, row=1)
+
+calculation = Label(text=0)
+calculation.grid(column=1, row=1)
+
+km_label = Label(text="Km")
+km_label.grid(column=2, row=1)
+
+calculate_btn = Button(text="Calculate", command=calculate)
+calculate_btn.grid(column=1, row=2)
+
+window.mainloop()
